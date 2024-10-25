d47d830f6f2eb6985631e2a5af931d606dfefd29
Guilherme <a14694@oficina.pt>
2024-10-25 16:52:02 +0100

    Update Projeto Final.py

--- Projeto Final.py
+++ Projeto Final.py
@@ -1,4 +1,4 @@
-#Projeto Final 
+#Projeto Final  
 
 introducao = "Bem vindo á floresta encantada"
 print(introducao)
@@ -8,7 +8,10 @@ sorte = "Desejo-te boa sorte!"
 print(sorte)
 escolha = input(f"Queres ir para o caminho da direita ou da esquerda?: ")
 print(escolha)
-direita = (f"Como escolheste o caminho da direita vais encontrar monstros")
-print(direita)
-esquerda = (f"Se escolheste o caminho da esquerda tás safo!")
-print(esquerda)
+if escolha == "direita":
+  print(f"Como escolheste o caminho da direita vais encontrar monstros")
+else:
+  print(f"Se escolheste o caminho da esquerda tás safo!")
+caminhos = input(f"Depois de escolheres o caminho da esquerda, vais ter mais 2 caminhos e se fores para a esquerda morres, se fores para a direita vais continuar o teu caminho tranquilo")
+if caminhos == "caminhos":
+  print(f"")
