e942147be62bb9f7a68800068b2e38c3da954d8e
Guilherme <a14694@oficina.pt>
2024-10-28 12:54:17 +0000

    Update Projeto Final.py

--- Projeto Final.py
+++ Projeto Final.py
@@ -1,17 +1,27 @@
-#Projeto Final  
+#Projeto Final 
 
+#Introdução 
 introducao = "Bem vindo á floresta encantada"
 print(introducao)
-nome = input(f"Qual é o teu nome?: ")
-print(nome)
+#Perguntar o nome e deseja-lhe boa sorte
+nome = input("Qual é o teu nome?: ")
 sorte = "Desejo-te boa sorte!"
 print(sorte)
-escolha = input(f"Queres ir para o caminho da direita ou da esquerda?: ")
-print(escolha)
+#Perguntar qual o caminho que quer seguir 
+escolha = input("Queres ir para o caminho da direita ou da esquerda?: ")
 if escolha == "direita":
-  print(f"Como escolheste o caminho da direita vais encontrar monstros")
-else:
-  print(f"Se escolheste o caminho da esquerda tás safo!")
-caminhos = input(f"Depois de escolheres o caminho da esquerda, vais ter mais 2 caminhos e se fores para a esquerda morres, se fores para a direita vais continuar o teu caminho tranquilo")
-if caminhos == "caminhos":
-  print(f"")
+  print("Os monstros vão te atacar!")
+  escolha = input("Queres fugir ou atacar?: ")
+  if escolha == "fugir":
+    print("Se fugires tás safo!")
+    if escolha == "atacar":
+      print("Se atacares vais morrer. GAME OVER!")
+
+if escolha == "esquerda": 
+  escolha = input("Vais ter mais 2 caminhos, qual vais escolher?: ")
+  if escolha == "esquerda": 
+    print("Escolheste o caminho errado. GAME OVER!")
+    if escolha == "direita":
+      print("Podes continuar o teu caminho sem preocupação!")
+#Fim de jogo 
+print(f"Fim de jogo {nome}!") 
