750f4820ed7955deaf4302b91251c30921821b90
Guilherme <a14694@oficina.pt>
2024-10-31 14:54:43 +0000

    Update Projeto Final.py

--- Projeto Final.py
+++ Projeto Final.py
@@ -9,19 +9,22 @@ sorte = "Desejo-te boa sorte!"
 print(sorte)
 #Perguntar qual o caminho que quer seguir 
 escolha = input("Queres ir para o caminho da direita ou da esquerda?: ")
+#Escolha do caminho
 if escolha == "direita":
   print("Os monstros vão te atacar!")
+#Escolhe se ataca ou foge dos monstros
   escolha = input("Queres fugir ou atacar?: ")
   if escolha == "fugir":
-    print("Se fugires tás safo!")
-    if escolha == "atacar":
-      print("Se atacares vais morrer. GAME OVER!")
+    print("Se conseguires fugir tás safo!")
+  if escolha == "atacar":
+    print("Se atacares vais morrer. GAME OVER!")
 
 if escolha == "esquerda": 
+#Escolha de mais 2 caminhos 
   escolha = input("Vais ter mais 2 caminhos, qual vais escolher?: ")
   if escolha == "esquerda": 
     print("Escolheste o caminho errado. GAME OVER!")
-    if escolha == "direita":
-      print("Podes continuar o teu caminho sem preocupação!")
-#Fim de jogo 
+  if escolha == "direita":
+    print("Podes continuar o teu caminho sem preocupação!")
+ #Fim de jogo 
 print(f"Fim de jogo {nome}!") 
